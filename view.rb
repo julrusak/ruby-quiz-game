e268ee7711ac1113998dcdcfa8f5f2e38007f347
julrusak <juliannarusak@gmail.com>
2015-11-11 19:26:17 -0500

    Add response to answer methods

--- view.rb
+++ view.rb
@@ -14,5 +14,14 @@ class View
   def greeting
     clear_screen
     puts "\n Do you know your U.S. State capitals?\n\n Ready to find out?\n Press enter!"
+    gets_user_input
+  end
+
+  def correct_answer
+    puts "Correct!"
+  end
+
+  def incorrect_answer
+    puts "Incorrect!"
   end
 end
