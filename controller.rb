f74a55e4025429863c68c708dda4585555f07d5f
julrusak <juliannarusak@gmail.com>
2015-11-11 20:43:51 -0500

    Add begin game controller method

--- controller.rb
+++ controller.rb
@@ -3,10 +3,17 @@ require_relative ('model')
 
 
 class QuizGame
+attr_reader :view, :game
 
   def initialize
     @view = View.new
     @game = Game.new
+  end
+
+  def start
+    view.clear_screen
+    view.greeting
+    game.setup_game
   end
 
 
